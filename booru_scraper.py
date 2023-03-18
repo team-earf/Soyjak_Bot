496132862ea3a9fca9a8a721a7f20fae537a66d4
super-mad-piano <79267124+super-mad-piano@users.noreply.github.com>
2023-03-18 07:12:08 -0500

    clarified misinformation in the docstring

--- booru_scraper.py
+++ booru_scraper.py
@@ -1,6 +1,6 @@
 """
 This module scrapes every available soyjak file from 'booru.soy'. It saves the files to '/soyjaks/' and
-the posts' tags to '/soyjak_tags/'.
+the posts' tags to '/soyjak_tags/' (well, not yet).
 """
 
 import requests
