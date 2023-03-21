38bdd98933aec55e68dcc045db446009313d7149
mad_piano2 <yabokonton@gmail.com>
2023-03-20 22:11:02 -0500

    reformatted to pep8, added rechecker

--- booru_scraper.py
+++ booru_scraper.py
@@ -22,9 +22,11 @@ class BooruPost:
         self.page_url = f"http://booru.soy/post/view/{post_id}"
         self.page = requests.get(self.page_url)
         self.page_soup = BeautifulSoup(self.page.text, "html.parser")
-        self.image_url = self.page_soup.find("input", {"id": "text_image-src"}).get("value")
+        self.image_url = self.page_soup.find(
+            "input", {"id": "text_image-src"}).get("value")
         self.file_ext = self.image_url.split(".")[-1]
-        self.post_tags = self.page_soup.find("span", {"class": "view"}).text.split(" ")  # splits string into list
+        self.post_tags = self.page_soup.find(
+            "span", {"class": "view"}).text.split(" ")  # splits string into list
 
     def download(self):
         """
@@ -35,7 +37,8 @@ class BooruPost:
         response = requests.get(self.image_url)
 
         soyjak_directory = "soyjaks"
-        soyjak_filepath = os.path.join(soyjak_directory, f"Soyjak #{self.post_id}.{self.file_ext}")
+        soyjak_filepath = os.path.join(
+            soyjak_directory, f"Soyjak #{self.post_id}.{self.file_ext}")
 
         with open(soyjak_filepath, 'wb') as f:
             f.write(response.content)
@@ -49,7 +52,8 @@ class BooruPost:
 
         tag_library_number = int(self.post_id / 5000)
         tag_directory = "soyjak_tags"
-        tag_filepath = os.path.join(tag_directory, f"tags{tag_library_number}.json")
+        tag_filepath = os.path.join(
+            tag_directory, f"tags{tag_library_number}.json")
 
         if not os.path.exists(tag_filepath):
             tags = {f"{self.post_id}": self.post_tags}
@@ -73,10 +77,13 @@ def get_latest_booru_post():
     :return: int
     """
 
+    logging.info("Checking the booru for latest post id...")
+
     catalog_page_url = "http://booru.soy/post/list"
     catalog_page = requests.get(catalog_page_url)
     catalog_page_soup = BeautifulSoup(catalog_page.text, "html.parser")
-    catalog_image_list = catalog_page_soup.find("div", {"class": "shm-image-list"})
+    catalog_image_list = catalog_page_soup.find(
+        "div", {"class": "shm-image-list"})
     catalog_images = catalog_image_list.findChildren("a")
 
     latest_post_number = int(catalog_images[0]["data-post-id"])
@@ -89,6 +96,8 @@ def number_to_start_off():
     """
     Looks at soyjaks that were already downloaded (if any) to determing where to start back on the booru.
     """
+    logging.info("Checking for previously downloaded soyjaks...")
+
     downloaded_soyjaks = os.listdir("soyjaks")
 
     if not downloaded_soyjaks:
@@ -97,35 +106,80 @@ def number_to_start_off():
         return soyjak_to_start_with
     else:
         logging.info(f"{len(downloaded_soyjaks):,d} already downloaded.")
-        downloaded_soyjak_numbers = [int(file.split("#")[1].split(".")[0]) for file in downloaded_soyjaks]
+        downloaded_soyjak_numbers = [
+            int(file.split("#")[1].split(".")[0]) for file in downloaded_soyjaks]
         last_downloaded_soyjak_number = max(downloaded_soyjak_numbers)
         soyjak_to_start_with = last_downloaded_soyjak_number + 1
-
-        soyjaks_unavailable = soyjak_to_start_with - last_downloaded_soyjak_number
-        logging.info(f"Due to various circumstances, {soyjaks_unavailable} are missing.")
-        logging.info("This is either due to local or Booru deletion.")
 
         logging.info(f"Starting with #{soyjak_to_start_with}")
         return soyjak_to_start_with
 
 
-def recheck_bad_soyjaks():
-    pass
+def scrape_post(post):
+    try:
+        booru_post = BooruPost(post)
+        booru_post.download()
+        logging.info(f"Download successful!")
+
+        booru_post.save_tags()
+        logging.info(f"Saved tags successfully!")
+    except:
+        logging.error(
+            f"Something wrong with #{post}, skipping...")
 
 
-# Script starts here if traditionally ran.
-if __name__ == "__main__":
+def the_big_scrape():
     post_limit = get_latest_booru_post()
     current_post_id = number_to_start_off()
 
     while current_post_id <= post_limit:
-        try:
-            booru_post = BooruPost(current_post_id)
-            booru_post.download()
-            logging.info(f"Download successful!")
-
-            booru_post.save_tags()
-            logging.info(f"Saved tags successfully!")
-        except:
-            logging.error(f"Something wrong with #{current_post_id}, skipping...")
+        scrape_post(current_post_id)
         current_post_id += 1
+
+    logging.info("You have all the soyjaks!")
+
+
+def recheck_missing_soyjaks():
+    """
+    If you want to recheck all the missing soyjaks, this function will go through each downloaded soyjak and check if there are any gaps.
+    """
+
+    while True:
+        recheck_input = input(
+            "Would you like to recheck all missing soyjaks? (y/n): ").lower()
+        if recheck_input == "y":
+            logging.info("Rechecking all missing soyjaks...")
+
+            # Gets numbers of all soyjaks that are downloaded in /soyjaks/
+            soyjak_list = os.listdir("soyjaks")
+            soyjak_list_numbers = [int(file.split("#")[1].split(".")[0])
+                                   for file in soyjak_list]
+
+            # Checks the gaps between each downloaded soyjak.
+            for i in range(1, max(soyjak_list_numbers)):
+                logging.info(f"Checking #{i}...")
+
+                current_post = soyjak_list_numbers[i]
+                next_post = soyjak_list_numbers[i + 1]
+                post_gap = next_post - current_post
+                logging.info(f"{post_gap} soyjaks missing.")
+
+                if post_gap > 1:
+                    posts_to_check = list(range(current_post + 1, next_post))
+                    for post in posts_to_check:
+                        scrape_post(post)
+
+            logging.info("Recheck complete!")
+
+        elif recheck_input == "n":
+            logging.info("Exiting...")
+            break
+
+        else:
+            logging.error("Invalid input!")
+
+
+# Script starts here if traditionally ran.
+if __name__ == "__main__":
+    the_big_scrape()
+    recheck_missing_soyjaks()
