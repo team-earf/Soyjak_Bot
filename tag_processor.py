e614025cdca8d91f7cc0c07e80a4650dad44502d
mad_piano2 <yabokonton@gmail.com>
2023-03-20 22:11:20 -0500

    added check if the alphebatized dir even exists

--- tag_processor.py
+++ tag_processor.py
@@ -11,8 +11,16 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
 # Makes list of all tag libraries, excluding any subdirectories and irrelevant files.
 tag_directory = "soyjak_tags"
-tag_libraries = [file for file in os.listdir(tag_directory) if file.startswith("tags")]
-alphabetized_tag_libraries = os.listdir(os.path.join(tag_directory, "alphabetized"))
+tag_libraries = [file for file in os.listdir(
+    tag_directory) if file.startswith("tags")]
+
+# Points to the alphabetical tag library directory.
+try:
+    alphabetized_tag_libraries = os.listdir(
+        os.path.join(tag_directory, "alphabetized"))
+# Or makes it if it doesn't exist.
+except:
+    os.makedirs(os.path.join(tag_directory, "alphabetized"))
 
 # Iterates through each tag library.
 logging.info("Processing tag libraries...")
@@ -36,14 +44,17 @@ for file in tag_libraries:
             # Checks if the first letter is a letter or number. If it isn't, it's set to "other".
             pattern = re.compile("[a-zA-Z0-9]")
             if not pattern.match(first_letter):
-                logging.info(f"Tag starts with non-alphanumeric character: {tag}\nSorting to 'other'.")
+                logging.info(
+                    f"Tag starts with non-alphanumeric character: {tag}\nSorting to 'other'.")
                 first_letter = "other"
 
-            alphabetical_tag_library_dir = os.path.join(tag_directory, "alphabetized")
+            alphabetical_tag_library_dir = os.path.join(
+                tag_directory, "alphabetized")
             if not os.path.exists(alphabetical_tag_library_dir):
                 os.makedirs(alphabetical_tag_library_dir)
 
-            alphabetical_tag_library_filepath = os.path.join(alphabetical_tag_library_dir, first_letter + ".json")
+            alphabetical_tag_library_filepath = os.path.join(
+                alphabetical_tag_library_dir, first_letter + ".json")
 
             # Checks if the alphabetical tag library exists.
             if os.path.exists(alphabetical_tag_library_filepath):
@@ -52,10 +63,12 @@ for file in tag_libraries:
 
                 # Checks if the tag exists in the alphabetical tag library.
                 if tag not in alphabetical_tags:
-                    logging.info(f"Adding new tag to alphabetical tag library: {tag}")
+                    logging.info(
+                        f"Adding new tag to alphabetical tag library: {tag}")
                     alphabetical_tags[tag] = [post_id]
                 else:
-                    logging.info(f"Adding post to existing tag in alphabetical tag library: {tag}")
+                    logging.info(
+                        f"Adding post to existing tag in alphabetical tag library: {tag}")
                     alphabetical_tags[tag].append(post_id)
 
                 # Saves the alphabetical tag library.
@@ -64,7 +77,8 @@ for file in tag_libraries:
 
             # If the alphabetical tag library doesn't exist, it's created.
             else:
-                logging.info("Creating new alphabetical tag library: " + first_letter + ".json")
+                logging.info(
+                    "Creating new alphabetical tag library: " + first_letter + ".json")
                 with open(alphabetical_tag_library_filepath, "w") as f:
                     alphabetical_tags = {tag: [post_id]}
                     # And saves the alphabetical tag library.
